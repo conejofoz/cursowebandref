9f1be7059cdda1f2e83cfb7bf772cfe40ca9b8d1
conejofoz <conejofoz@gmail.com>
2018-08-14 11:56:21 -0300

    botao zap

---
+++ whatsapp.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "blaba";
+?>
+<a href="https://api.whatsapp.com/send?text=https://ecommerce.infinity-group.net/saladeira-de-acrilico-vermelha-ky1006-4r">infinity</a>
+
+
+<a href="https://api.whatsapp.com/send?l=pt&amp;phone=5545707070"><img src="https://i.imgur.com/XMsxHpu.png" style="height:80px; position:fixed; bottom: 25px; right: 25px; z-index:100;" data-selector="img"></a>
